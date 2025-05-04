3f541b38263e16d99dc45e743549bb7f84bc4a7d
wintermelyn <matas.seplveda@uc.cl>
2025-05-04 19:12:37 -0400

    ss

--- kavak_scraper/src/kavak_scraper/main.py
+++ kavak_scraper/src/kavak_scraper/main.py
@@ -121,7 +121,8 @@ def main():
     all_cars = []
 
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
+        browser = p.chromium.launch(channel="chrome", headless=True)
+
         page = browser.new_page(
             user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
             extra_http_headers={
--- kavak_scraper/start.sh
+++ kavak_scraper/start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 cd kavak_scraper
 poetry install --no-root --no-interaction
-poetry run playwright install chromium
-poetry run scrap
+poetry run scrapW
